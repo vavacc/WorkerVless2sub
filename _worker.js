f83937170f5a4f4e5d684e060c6a4b88d43e9443
vava <58034771+vavacc@users.noreply.github.com>
2025-11-17 20:33:33 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -28,7 +28,7 @@ let 临时中转域名 = [];
 let 临时中转域名接口 = '';
 let EndPS = '';
 let 协议类型 = atob(`\u0056\u006b\u0078\u0046\u0055\u0031\u004d\u003d`);
-let FileName = '优选订阅生成器';
+let FileName = 'AI乄幽紫订阅生成器';
 let SUBUpdateTime = 6;
 let total = 24;
 let timestamp = 4102329600000;
@@ -42,7 +42,7 @@ let MamaJustKilledAMan = ['telegram', 'twitter', 'miaoko'];
 let proxyIPPool = [];
 let socks5Data;
 let alpn = '';
-let 网络备案 = `<a href='https://t.me/CMLiussss'>萌ICP备-20240707号</a>`;//写你自己的维护者广告
+let 网络备案 = `<a href='https://vavacc.eu.org'>Sky | AI乄幽紫`;//写你自己的维护者广告
 let 额外ID = '0';
 let 加密方式 = 'auto';
 let 网站图标, 网站头像, 网站背景, xhttp = '';
@@ -1463,3 +1463,4 @@ export default {
 		}
 	}
 };
+
